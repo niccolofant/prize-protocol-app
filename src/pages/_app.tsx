d8fa2fbe0206e64cecd4f781e63d14316526a304
ocintnaf <ocintnaf@gmail.com>
2022-03-16 20:51:59 +0100

    Add Moralis authentication

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../assets/styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
 import type { AppProps } from 'next/app'
 
+const MORALIS_APP_ID = process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID
+const MORALIS_SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL
+
+console.log(MORALIS_APP_ID)
+
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  const isServerInfo = MORALIS_APP_ID && MORALIS_SERVER_URL ? true : false
+
+  if (!MORALIS_APP_ID || !MORALIS_SERVER_URL)
+    throw new Error(
+      'Missing Moralis Application ID or Server URL. Make sure to set your .env file.'
+    )
+  if (isServerInfo)
+    return (
+      <MoralisProvider appId={MORALIS_APP_ID} serverUrl={MORALIS_SERVER_URL}>
+        <Component {...pageProps} />
+      </MoralisProvider>
+    )
 }
 
 export default App
