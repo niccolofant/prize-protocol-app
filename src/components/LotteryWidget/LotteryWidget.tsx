df6ffca08ec62d7d05e3e4c554a98129c3175639
ocintnaf <ocintnaf@gmail.com>
2022-03-23 20:52:24 +0100

    Initial work

--- src/components/LotteryWidget/LotteryWidget.tsx
+++ src/components/LotteryWidget/LotteryWidget.tsx
@@ -1,29 +1,18 @@
 import {
   Avatar,
   Badge,
-  Button,
   Card,
   Divider,
   Skeleton,
   Statistic,
-  Tooltip,
   Typography,
 } from 'antd'
 import { gql, useQuery } from 'urql'
-import { getCompoundApy, getDrawingDate } from '../../utils/functions'
-import {
-  useWeb3ExecuteFunction,
-  useMoralis,
-  useMoralisQuery,
-  useApiContract,
-  useChain,
-} from 'react-moralis'
-import { c2, n4, n6 } from '../../utils/formatters'
+import { getDrawingDate } from '../../utils/functions'
+import { useMoralis, useWeb3Contract, useChain } from 'react-moralis'
+import { n4 } from '../../utils/formatters'
 import { Compound, CUSDT, USDT } from '../Images/Images'
-import { useCallback, useEffect } from 'react'
 import { prizeProtocolABI } from '../../utils/abis/prizeProtocolABI'
-import { usdtABI } from '../../utils/abis/usdtABI'
-import { cUsdtABI } from '../../utils/abis/cUsdtABI'
 import Link from 'next/link'
 
 const query = gql`
@@ -63,24 +52,27 @@ const LotteryWidget = () => {
   const lotteryInfo = protocolInfo?.lotteries[0]
 
   const {
-    runContractFunction,
-    data: supplyRate,
+    data: prizePool,
     error,
-    isLoading,
+    runContractFunction,
     isFetching,
-  } = useApiContract({
-    address: protocolInfo?.cToken,
-    functionName: 'supplyRatePerBlock',
-    abi: cUsdtABI,
-    chain: '0x4',
+    isLoading,
+  } = useWeb3Contract({
+    abi: prizeProtocolABI,
+    contractAddress: protocolInfo?.address,
+    functionName: 'prizePool',
+    params: {},
   })
 
-  console.log(data)
+  const handleSuccess = async (tx: any) => {
+    await tx.wait(1)
+    console.log(tx)
+  }
 
   if (fetching) return <Skeleton />
   return (
     <Card className="rounded-xl">
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-center">
         <div className="flex items-center space-x-4">
           <Avatar.Group>
             <Avatar src={<CUSDT />} />
@@ -95,7 +87,7 @@ const LotteryWidget = () => {
 
       <Divider />
 
-      <div className="flex items-center justify-between space-x-10">
+      <div className="flex items-center justify-evenly space-x-10">
         <div className="flex flex-col items-center space-y-2">
           <Typography className="text-slate-500">Prize Pool</Typography>
           <Typography className="text-5xl font-semibold text-slate-800">
@@ -118,7 +110,7 @@ const LotteryWidget = () => {
 
       <Divider />
 
-      <div className="flex items-center justify-between">
+      <div className="flex items-center justify-evenly">
         <div className="text-center">
           <Typography className="text-slate-500">Yield protocol</Typography>
           <Avatar src={<Compound />} />
@@ -127,6 +119,16 @@ const LotteryWidget = () => {
           <button className="border px-10">Deposit</button>
         </Link>
       </div>
+      <button
+        onClick={async () =>
+          await runContractFunction({
+            onSuccess: handleSuccess,
+          })
+        }
+      >
+        AAAAA
+      </button>
+      {prizePool && JSON.stringify(prizePool)}
     </Card>
   )
 }
