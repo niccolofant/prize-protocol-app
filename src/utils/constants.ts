669c3eb7de90750d6028f1597c42a29c67f605b2
ocintnaf <ocintnaf@gmail.com>
2022-03-24 08:30:57 +0100

    Add addresses constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+export const USDT_ADDRESS = '0xd9ba894e0097f8cc2bbc9d24d308b98e36dc6d02'
+export const USDT_NAME = 'USDC'
+export const CUSDT_ADDRESS = '0x2fb298bdbef468638ad6653ff8376575ea41e768'
+export const TICKET_ADDRESS = '0x95d28ae99f5b32abb1ae477ab31b2b94be2aab9a'
+export const PROTOCOL_ADDRESS = '0xdd36B97dEFe96515fA2463b235dc237474ddE17f'
