4ff749fe8ccb5ec3c46c41b52a4071a4736d4dba
ocintnaf <ocintnaf@gmail.com>
2022-03-29 20:43:14 +0200

    Add contract's etherscan link and change style

--- src/pages/[address]/deposit.tsx
+++ src/pages/[address]/deposit.tsx
@@ -1,6 +1,8 @@
 import { Avatar } from 'antd'
 import { NextPage } from 'next'
+import Link from 'next/link'
 import React, { useCallback, useState } from 'react'
+import { gql, useQuery } from 'urql'
 import DepositModal from '../../components/DepositModal/DepositModal'
 import DepositorStats from '../../components/DepositorStats/DepositorStats'
 import DepositStats from '../../components/DepositStats/DepositStats'
@@ -8,15 +10,27 @@ import { CUSDT, USDT } from '../../components/Images/Images'
 import Layout from '../../components/Layout/Layout'
 import LotteryStats from '../../components/LotteryStats/LotteryStats'
 import PrizeStats from '../../components/PrizeStats/PrizeStats'
+import { getEllipsisTxt } from '../../utils/formatters'
+
+const protocolNameQuery = gql`
+  query {
+    prizeProtocols(first: 1) {
+      address
+    }
+  }
+`
 
 const deposit: NextPage = () => {
   const [isModalVisible, setIsModalVisible] = useState(false)
+  const [{ data }] = useQuery({ query: protocolNameQuery })
+
+  const address: string = data?.prizeProtocols[0].address
 
   const handleButtonClick = useCallback(() => {
     setIsModalVisible(true)
   }, [])
 
-  const handleClose = useCallback(() => {
+  const onClose = useCallback(() => {
     setIsModalVisible(false)
   }, [])
 
@@ -24,37 +38,46 @@ const deposit: NextPage = () => {
     <Layout>
       <div>
         <div className="space-y-10">
-          <div className="grid grid-cols-1 space-y-5 bg-red-300 sm:grid-cols-2">
-            <div className="flex items-center justify-center space-x-4 sm:justify-start">
+          <div className="grid grid-cols-1 items-center justify-between space-y-5 sm:grid-cols-3 sm:space-y-0">
+            <div className="col-span-2 flex items-center justify-start space-x-4 sm:justify-start">
               <Avatar.Group>
                 <Avatar src={<CUSDT />} />
                 <Avatar src={<USDT />} />
               </Avatar.Group>
-              <h1 className="text-2xl font-semibold text-prize-dark-gray md:text-4xl">
-                Prize Lottery USDT
-              </h1>
+              <div className="space-y-1">
+                <h1 className="text-2xl font-semibold text-prize-dark-gray md:text-4xl">
+                  Prize Lottery USDT
+                </h1>
+                <h3>
+                  <span className="text-prize-light-gray">Address: </span>
+                  {address && (
+                    <Link
+                      href={`https://https://rinkeby.etherscan.io/address/${address}`}
+                    >
+                      <a
+                        className="font-semibold text-prize-dark-gray hover:text-prize-dark-gray"
+                        target="_blank"
+                      >
+                        {getEllipsisTxt(address, 6)}
+                      </a>
+                    </Link>
+                  )}
+                </h3>
+              </div>
             </div>
-            <div className="">
-              <button
-                className="rounded-lg bg-prize-red px-16 py-3 font-semibold text-white shadow-xl"
-                onClick={handleButtonClick}
-              >
-                Deposit
-              </button>
-              <button
-                className="rounded-lg border bg-white px-16 py-3 
-              font-semibold text-prize-dark-gray shadow-xl"
-              >
-                Redeem
-              </button>
-            </div>
+            <button
+              className="rounded-lg bg-prize-red py-2 px-10 text-sm font-semibold text-white shadow-xl sm:text-base"
+              onClick={handleButtonClick}
+            >
+              Deposit
+            </button>
           </div>
           <LotteryStats />
           <PrizeStats />
           <DepositStats />
           <DepositorStats />
         </div>
-        <DepositModal visible={isModalVisible} handleClose={handleClose} />
+        <DepositModal visible={isModalVisible} onClose={onClose} />
       </div>
     </Layout>
   )
