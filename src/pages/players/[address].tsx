a0d422a6f7fcd6b0842a97a4e6af4901d8c3c81c
ocintnaf <ocintnaf@gmail.com>
2022-03-31 21:36:48 +0200

    Initial work for the player page

--- src/pages/players/[address].tsx
+++ src/pages/players/[address].tsx
@@ -1,5 +1,70 @@
-const Player = () => {
-  return <div>PLAYER</div>
+import { FunctionComponent } from 'react'
+import { gql, useQuery } from 'urql'
+import Layout from '../../components/Layout/Layout'
+import PlayerStats from '../../components/PlayerStats/PlayerStats'
+
+export interface PlayerProps {
+  query: {
+    address: string
+  }
+}
+
+export interface PlayerInfo {
+  player: {
+    balance: string
+  }
+  wins: {
+    id: string
+    timestamp: string
+    winner: {
+      address: string
+    }
+    amount: string
+  }[]
+}
+
+const playerInfoQuery = gql`
+  query ($id: String) {
+    player(id: $id) {
+      balance
+    }
+    wins(orderBy: timestamp, orderDirection: desc, first: 1) {
+      id
+      timestamp
+      winner {
+        address
+      }
+      amount
+    }
+  }
+`
+
+const Player: FunctionComponent<PlayerProps> = ({ query }) => {
+  const [{ data }] = useQuery<PlayerInfo>({
+    query: playerInfoQuery,
+    variables: { id: query.address },
+  })
+
+  return (
+    <Layout>
+      <div>
+        <div className="space-y-10">
+          <div>
+            <h1 className="text-2xl font-semibold text-prize-dark-gray md:text-4xl">
+              My Account
+            </h1>
+            <h3 className="text-prize-light-gray">Account overview</h3>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export const getServerSideProps = async ({ query }: PlayerProps) => {
+  return {
+    props: { query },
+  }
 }
 
 export default Player
